10f2c992f9cac4c18b1fb1a74dd62e35efaa8e8b
Hardikag17 <54525688+Hardikag17@users.noreply.github.com>
2021-04-18 00:54:33 +0530

    center form

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -6,36 +6,38 @@ import logo from '../images/logo.png';
 function SignIn() {
   return (
     <main class='form-signin'>
-      <form>
-        <img class='mb-4' src={logo} styles={{ width: '7', height: '5' }} />
-        <h1 class='h3 mb-3 fw-normal'>Please sign in</h1>
+      <center>
+        <form>
+          <img class='mb-4' src={logo} styles={{ width: '7', height: '5' }} />
+          <h1 class='h3 mb-3 fw-normal'>Please sign in</h1>
 
-        <div class='form-floating'>
-          <input
-            type='email'
-            class='form-control'
-            id='floatingInput'
-            placeholder='name@example.com'></input>
-          <label for='floatingInput'>Email address</label>
-        </div>
-        <div class='form-floating'>
-          <input
-            type='password'
-            class='form-control'
-            id='floatingPassword'
-            placeholder='Password'></input>
-          <label for='floatingPassword'>Password</label>
-        </div>
-        <div class='checkbox mb-3'>
-          <label>
-            <input type='checkbox' value='remember-me' /> Remember me
-          </label>
-        </div>
-        <button class='w-100 btn btn-lg btn-primary' type='submit'>
-          Sign in
-        </button>
-        <p class='mt-5 mb-3 text-muted'>&copy; 2017–2021</p>
-      </form>
+          <div class='form-floating'>
+            <input
+              type='email'
+              class='form-control'
+              id='floatingInput'
+              placeholder='name@example.com'></input>
+            <label for='floatingInput'>Email address</label>
+          </div>
+          <div class='form-floating'>
+            <input
+              type='password'
+              class='form-control'
+              id='floatingPassword'
+              placeholder='Password'></input>
+            <label for='floatingPassword'>Password</label>
+          </div>
+          <div class='checkbox mb-3'>
+            <label>
+              <input type='checkbox' value='remember-me' /> Remember me
+            </label>
+          </div>
+          <button class='w-100 btn btn-lg btn-primary' type='submit'>
+            Sign in
+          </button>
+          <p class='mt-5 mb-3 text-muted'>&copy; 2021–2022</p>
+        </form>
+      </center>
     </main>
   );
 }
