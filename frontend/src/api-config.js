0efc69361939bcad2c9339c4448b817704aebfc2
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-04-18 19:48:26 +0530

    add production backend

--- frontend/src/api-config.js
+++ frontend/src/api-config.js
@@ -1,4 +1,4 @@
 export const API_ROOT =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8081' //dev
-    : 'http://localhost:8081'; //prod
+    : 'https://pawsadoption.herokuapp.com'; //prod
