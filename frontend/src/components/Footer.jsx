e57701bf47f4f1740beaa32526cbfeeb55ba0045
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-04-18 19:07:00 +0530

    theme footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,76 +1,93 @@
-import '../styles/Footer.css';
 import logo from '../images/logo.png';
-import facebook from '../images/facebook .png';
-import twitter from '../images/twitter.png';
-import instagram from '../images/instagram .png';
 function Footer() {
   return (
     <div id='footer'>
-      <footer className='footer1'>
-        <div>
-          <span>© 2021 Copyright</span>
+      <footer class='bg-light text-center text-black'>
+        {/* <!-- Grid container --> */}
+        <div class='container p-4'>
+          <section class='mb-4'>
+            <h4>About Us</h4>
+            <p>
+              ' Paws Adoption ' is a nonprofit animal shelter based out of Delhi
+              NCR. ' Paws Adoption ' is funded solely from adoption fees,
+              donations from the public, and our own fundraising efforts.
+              <br></br>
+              <br></br>
+              Our mission is to serve the people and animals of Delhi NCR and
+              neighbouring cities by providing safe, temporary shelter and care
+              for abandoned or otherwise homeless cats and dogs, and to reduce
+              pet overpopulation by means of spay and neuter, education, and
+              community outreach. We have been fulfilling our mission since
+              2021.
+            </p>
+          </section>
         </div>
 
-        <div className='d-flex justify-content-center line2'>
-          <div>
-            <img className='a' src={logo} alt='Logo' />
-          </div>
-
-          <div>
-            <center>
-              <table>
-                <tr>
-                  <td colSpan='3'> Join Us On</td>
-                </tr>
-                <tr>
-                  <td>
-                    <img src={facebook} alt='Facebook' />
-                  </td>
-                  <td>
-                    <img src={instagram} alt='Instagram' />
-                  </td>
-                  <td>
-                    <img src={twitter} alt='Twitter' />
-                  </td>
-                </tr>
-              </table>
-            </center>
-          </div>
-        </div>
-
-        <br></br>
-        <p className='line2'>
-          ' Paws Adoption ' is a nonprofit animal shelter based out of Delhi
-          NCR. ' Paws Adoption ' is funded solely from adoption fees, donations
-          from the public, and our own fundraising efforts.
-          <br></br>
-          <br></br>
-          Our mission is to serve the people and animals of Delhi NCR and
-          neighbouring cities by providing safe, temporary shelter and care for
-          abandoned or otherwise homeless cats and dogs, and to reduce pet
-          overpopulation by means of spay and neuter, education, and community
-          outreach. We have been fulfilling our mission since 2021.
-        </p>
         <div className='container-fluid'>
-          <h5>Contact Us</h5>
+          <h4>Contact Us</h4>
           <div className='row row-12'>
-            <div className='col col-md-4'>
+            <div className='col col-md-4 p-3'>
               <div> Hardik Agarwal</div>
-              <a href='mailto:hardikag17@gmil.com'>hardikag17@gmil.com</a>
+              <a href='mailto:hardikag17@gmil.com' className='text-black'>
+                hardikag17@gmil.com
+              </a>
             </div>
-            <div className='col-md-4'>
+            <div className='col-md-4 p-3'>
               <div> Suryashankar Das</div>
-              <a href='mailto:hardikag17@gmil.com'>
+              <a
+                href='mailto:suryashankardas.2002@gmil.com'
+                className='text-black'>
                 suryashankardas.2002@gmil.com
               </a>
             </div>
-            <div className='col-md-4'>
+            <div className='col-md-4 p-3'>
               <div> Aman Nagpal</div>
-              <a href='mailto:amannagpal2468@gmail.com'>
+              <a href='mailto:amannagpal2468@gmail.com' className='text-black'>
                 amannagpal2468@gmail.com
               </a>
             </div>
           </div>
+        </div>
+        <section class='mb-4'>
+          <a
+            class='btn btn-outline-dark btn-floating m-1'
+            href='#!'
+            role='button'>
+            <i class='fab fa-facebook-f'></i>
+          </a>
+
+          <a
+            class='btn btn-outline-dark btn-floating m-1'
+            href='#!'
+            role='button'>
+            <i class='fab fa-twitter'></i>
+          </a>
+
+          <a
+            class='btn btn-outline-dark btn-floating m-1'
+            href='#!'
+            role='button'>
+            <i class='fab fa-instagram'></i>
+          </a>
+          <a
+            class='btn btn-outline-dark btn-floating m-1'
+            href='#!'
+            role='button'>
+            <i class='fab fa-github'></i>
+          </a>
+        </section>
+        <center>
+          <img
+            className='a'
+            src={logo}
+            alt='Logo'
+            // style={{ maxHeight: '100px', outerWidth: 'auto' }}
+          />
+        </center>
+        <div
+          class='text-center p-3'
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+          © 2021 Copyright: Team DNA
         </div>
       </footer>
     </div>
